c74b3f3e9afd6676609ada265325c6f3bfbdcceb
Soraya <srodriguezoj@uoc.edu>
2025-05-19 17:53:56 +0200

    save

--- config/database.php
+++ config/database.php
@@ -56,9 +56,9 @@ return [
                 'strict' => true,
                 'engine' => null,
                 'options' => extension_loaded('pdo_mysql') ? array_filter([
-                    PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA'),
-                    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                ]) : [],
+                        PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA'),
+                        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                    ]) : [],
             ],
 
 				
